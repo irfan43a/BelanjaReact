5fa78e07a098759d9d8b52c0ca6967d09ae893a7
Irfan Abidin <irfan43abdin@gmail.com>
2022-07-27 09:59:11 +0700

    add redux get and detail3

--- src/components/modules/Card/index.js
+++ src/components/modules/Card/index.js
@@ -1,9 +1,31 @@
 import React from "react";
+import styles from "./card.module.css";
+import star from "../../../img/Star.png";
+import { Link } from "react-router-dom";
 
-const Card = ({ content }) => {
+const Card = ({ image, name, price, id }) => {
   return (
-    <div class="card" style={{ width: "200px" }}>
-      <div class="card-body">{content}</div>
+    <div className={styles.card}>
+      <div className={styles.imgcontainer}>
+        <img src={image} alt={image} />
+      </div>
+      <div className={styles.card_title}>
+        <Link to={`/products/${id}`}>{name}</Link>
+      </div>
+      <div className={styles.deskrip}>
+        <div className={styles.lain}>
+          <div className={styles.price}>$ {price}</div>
+          <div className={styles.store}> Zalora Cloth</div>
+          <div className={styles.store_rangked}>
+            <img src={star} alt="start" />
+            <img src={star} alt="start" />
+            <img src={star} alt="start" />
+            <img src={star} alt="start" />
+            <img src={star} alt="start" />
+            (10)
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
